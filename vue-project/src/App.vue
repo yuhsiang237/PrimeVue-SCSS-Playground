e4f20be195194bd3fa883a788c6d91c0c851c904
yu hsiang <175248322+tomlin3193@users.noreply.github.com>
2026-01-08 17:13:09 +0800

    feat: apply PrimeVue table

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,40 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Button from "primevue/button"
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+
+
+import { ref, onMounted } from 'vue';
+import { CustomerService } from './services/customService';
+
+onMounted(() => {
+    CustomerService.getCustomersMedium().then((data) => (customers.value = data));
+});
+
+const customers = ref();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Table test</h1>
+    <DataTable :value="customers" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" tableStyle="min-width: 50rem"
+        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        currentPageReportTemplate="{first} to {last} of {totalRecords}">
+    <template #paginatorstart>
+        <Button type="button" icon="pi pi-refresh" text />
+    </template>
+    <template #paginatorend>
+        <Button type="button" icon="pi pi-download" text />
+    </template>
+    <Column field="name" header="Name" style="width: 25%"></Column>
+    <Column field="country.name" header="Country" style="width: 25%"></Column>
+    <Column field="company" header="Company" style="width: 25%"></Column>
+    <Column field="representative.name" header="Representative" style="width: 25%"></Column>
+</DataTable>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
